89b45a0e8cd5997b1db516eeb24f9df896cb0a96
NicholasSeward <nicholas.seward@gmail.com>
2019-09-11 13:45:11 -0500

    added template for assignment 5

--- Assignment04/Program.py
+++ Assignment04/Program.py
@@ -1,1 +1,48 @@
-print("hello world")
+import turtle,math
+
+#~ bob=turtle.Turtle()
+#~ #tell the turtle what to do
+#~ bob.forward(100)
+#~ bob.left(90)
+#~ bob.forward(100)
+#~ bob.left(90)
+#~ bob.forward(100)
+#~ bob.left(90)
+#~ bob.forward(100)
+
+#~ screen=bob.getscreen()
+#~ screen.mainloop()
+
+#functions/methods
+#math function
+#  f(x)->y
+#  input ->box-> output
+#methods
+# input ->box 
+
+
+def printHellos(name,name2):
+    """
+    Prints hello to two people.
+    
+    Variables:
+    name - first person's name
+    name2 - secon person's name
+    """
+    print("hello "+str(name))
+    print("hello "+str(name2))
+
+
+def sqrt(n):
+    if n<0:
+        raise ValueError("the value must be non-negative")
+    print(math.sqrt(n))
+
+name=input("what is your name? ")
+printHellos(name,7)
+sqrt(-10)
+#this line
+
+
+
+
---
+++ Assignment04/TurtleExample.py
@@ -0,0 +1,14 @@
+import turtle
+
+bob=turtle.Turtle()
+#tell the turtle what to do
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+bob.left(90)
+bob.forward(100)
+
+screen=bob.getscreen()
+screen.mainloop()
